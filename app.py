efb4893aea1894c1962cd6c5a02caeebe6ae9a45
dant <antimonov94@gmai.com>
2020-02-19 11:17:23 +0500

    feat(app): add some functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect, jsonify, abort
+from flask import Flask, request, render_template, redirect, jsonify, abort, make_response
+from math import floor
 from sqlite3 import OperationalError
 import string
 import sqlite3
@@ -36,6 +37,88 @@ def table_check():
             pass
 
 
+# @app.route('/', methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'POST':
+#         original_url = str_encode(request.form.get('url'))
+#         if urlparse(original_url).scheme == '':
+#             url = 'http://' + original_url
+#         else:
+#             url = original_url
+#         with sqlite3.connect('urls.db') as conn:
+#             hashed_url = str(base64.urlsafe_b64encode(url)).replace("=", "").replace("\'", "")[-6:] + str(
+#                 random.randint(1, 10))
+#             cursor = conn.cursor()
+#             res = cursor.execute(
+#                 'INSERT INTO WEB_URL (HASH_URL, URL) VALUES (?, ?)',
+#                 [hashed_url, base64.urlsafe_b64encode(url)]
+#             )
+#         return render_template('home.html', short_url=host + hashed_url)
+#     return render_template('home.html')
+#
+#
+# @app.route('/<short_url>')
+# def redirect_short_url(short_url):
+#     print(short_url)
+#     url = host  # fallback if no URL is found
+#     with sqlite3.connect('urls.db') as conn:
+#         cursor = conn.cursor()
+#         res = cursor.execute('SELECT URL FROM WEB_URL WHERE HASH_URL=?', [short_url])
+#         try:
+#             short = res.fetchone()
+#             if short is not None:
+#                 url = base64.urlsafe_b64decode(short[0])
+#         except Exception as e:
+#             print(e)
+#     return redirect(url)
+#
+#
+# tasks = [
+#     {
+#         'id': 1,
+#         'title': u'Buy groceries',
+#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'title': u'Learn Python',
+#         'description': u'Need to find a good Python tutorial on the web',
+#         'done': False
+#     }
+# ]
+
+
+# @app.route('/api/tasks', methods=['GET'])
+# def get_tasks():
+#     return jsonify({'tasks': tasks})
+#
+#
+# @app.route('/api/tasks/<int:task_id>', methods=['GET'])
+# def get_task(task_id):
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify({'task': task[0]})
+#
+#
+# @app.errorhandler(404)
+# def not_found(error):
+#     return make_response(jsonify({'error': 'Not found'}), 404)
+#
+#
+# @app.route('/api/tasks', methods=['POST'])
+# def create_task():
+#     if not request.json or not 'title' in request.json:
+#         abort(400)
+#     task = {
+#         'id': tasks[-1]['id'] + 1,
+#         'title': request.json['title'],
+#         'description': request.json.get('description', ""),
+#         'done': False
+#     }
+#     tasks.append(task)
+#     return jsonify({'task': task}), 201
 def check_if_url_in_db(url):
     with sqlite3.connect('urls.db') as conn:
         cursor = conn.cursor()
@@ -74,9 +157,10 @@ def post_short_url():
     else:
         url = original_url
         try:
-            return jsonify({'short_url': check_if_url_in_db(url)})
+            return jsonify({'hash': check_if_url_in_db(url)})
         except:
-            return jsonify({'short_url': insert_into_db(url)})
+            return jsonify({'hash': insert_into_db(url)})
+
 
 
 @app.route('/<short_url>')
